218970868d5fc10a0c60d5f161db01a12941e797
PeterVillanueva <peterdonniel4444@gmail.com>
2024-02-10 19:03:16 +0800

    Commit

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -11,7 +11,7 @@ function NavBar() {
         <div className="text-white text-lg font-medium font-Poppins hover:text-sky-300 hover:shadow-sky-400 transition-all ease-in">Contact us</div>
       </div>
 
-      <button className="px-6 py-2 bg-gradient-to-r from-slate-700 to-slate-600 hover:bg-gradient-to-r hover:from-slate-800 hover:to-slate-700 hover:translate-y-[2px] transition-all ease-in-out rounded-md shadow-lg text-white text-lg font-medium font-Poppins">Sign up</button>
+      <a href="https://petervillanueva.github.io/Modern-Weather-Main/" className="px-6 py-2 bg-gradient-to-r from-slate-700 to-slate-600 hover:bg-gradient-to-r hover:from-slate-800 hover:to-slate-700 hover:translate-y-[2px] transition-all ease-in-out rounded-md shadow-lg text-white text-lg font-medium font-Poppins">View Demo</a>
     </div>
   );
 }
